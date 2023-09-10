65f0ceefecb85664502e7ba6b748a49cbc3d46cc
Rashi Tiwari <117359006+RashiTiwari12@users.noreply.github.com>
2023-09-10 11:40:02 +0530

    navbar updated

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return (
         <>
             <div className={styles.mainDiv}>
-                <h1 className={styles.Title}>DevGenix</h1>
+                <h1 className={styles.Title}>DevGenZx</h1>
                 <div className={styles.NavOptions}>
                     <a className={styles.NavOption} href="#about">
                         <h3 >About</h3>
